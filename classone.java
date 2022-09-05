d5a84032955acaa7dbfb99427306499a9626c9fc
noorqen <noonaa2000@gmail.com>
2022-09-05 19:40:09 +0300

    done

---
+++ classone.java
@@ -0,0 +1,15 @@
+/* Online Java Compiler and Editor */
+public class HelloWorld{
+
+     public static void main(String []args){
+        int age = 22;
+        String name = "Noor";
+        System.out.println("Hi, My name is " + name + " and I am " + age + " years old");
+        int height = 160;
+        if (height >= 160){
+            System.out.println("you can enter");
+        } else{
+            System.out.println("you cannot enter");
+        }
+     }
+}
